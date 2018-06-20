993e8f7d4a17850f52d49279545431926550893b
Kalle Pusa <kalle.pusa@gmail.com>
2018-06-20 15:58:32 +0300

    Tässä toimiva peli

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import kayttis.KayttoliittymaR;
+
 import java.util.Scanner;
 
 public class Main {
@@ -5,19 +7,24 @@ public class Main {
 
         Scanner lukija = new Scanner(System.in);
         System.out.println("Mitä pelataan?");
+        System.out.println("Kallen peli, Reijan Peli vai Joonaksen peli?");
+
         String sana = lukija.nextLine();
 
-        if (sana.equals("Kallen peli")) {
-            Tekstikayttoliittyma t = new Tekstikayttoliittyma();
-            t.kaynnista();
+            if (sana.equals("Kallen peli")) {
+                Tekstikayttoliittyma t = new Tekstikayttoliittyma();
+                t.kaynnista();
+            }
+
+            if (sana.equals("Joonaksen peli")) {
+                Kayttoliittyma j = new Kayttoliittyma();
+                j.kaynnista();
+            }
+            if (sana.equals("Reijan peli")) {
+                KayttoliittymaR r = new KayttoliittymaR();
+                r.kaynnista();
+            }
         }
-        if (sana.equals("Joonaksen peli")) {
-            Kayttoliittyma j = new Kayttoliittyma();
-            j.kaynnista();
-        }
-		if (sana.equals("Reijan peli")) {
-			KayttoliittymaR r = new KayttoliittymaR();
-			r.kaynnista();
+
     }
-	
-}
+
