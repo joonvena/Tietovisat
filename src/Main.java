e90304638e80ce7b4865385c796b38dfa3ffe614
Kalle Pusa <kalle.pusa@gmail.com>
2018-06-20 13:51:16 +0300

    Kolmas muutos, lisätty Mainiin pelien valintavaihtoehdot

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+		Scanner lukija = new Scanner(System.in);
+		System.out.println("Mitä pelataan?");
+		String mitapelataan = lukija.nextLine();
+		if(mitapelataan.equals("Kallen peli")) {
         Tekstikayttoliittyma t = new Tekstikayttoliittyma();
         t.kaynnista();
-
+		}
+		if(mitapelataan.equals("Joonaksen peli")) {
+			//käynnistä Joonaksen peli
+		}
+		if(mitapelataan.equals("Reijan peli")) {
+			
+		}
 
     }
 }
